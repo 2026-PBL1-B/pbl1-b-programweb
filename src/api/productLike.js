d46776fb1bd1d0be4313df1c90a149eb5a6127c2
樋口冬舞 <kd1366263@st.kobedenshi.ac.jp>
2026-04-24 14:01:07 +0900

    制作物いいねテーブル取得

--- src/api/productLike.js
+++ src/api/productLike.js
@@ -1,5 +1,4 @@
 import { supabase } from '../spabase'
-
 /**
  * SpabaseのProductLikeテーブルに新しいいいね情報を追加する関数
  * @param {string} product_id いいねを追加する制作物のID
@@ -9,9 +8,7 @@ import { supabase } from '../spabase'
 export async function postProductLike(product_id, user_id) {
   const { data, error } = await supabase
     .from('ProductLike')
-    .insert([
-      { product_id: product_id, user_id: user_id },
-    ]);
+    .insert([ { product_id: product_id, user_id: user_id },]);
 
   if (error) {
     console.error('いいねの追加に失敗:', error.message);
@@ -19,3 +16,31 @@ export async function postProductLike(product_id, user_id) {
     console.log('いいねが追加に成功:', data);
   }
 }
+
+/**
+ * SupabaseのProductLikeテーブルからすべてのいいね情報を取得する関数
+ */
+
+export async function getProductLike() {
+
+  // .from('ProductLike') : いいねを保存しているテーブルを指定
+  // .select('*') : 全てのカラムを取得
+  const { data, error } = await supabase
+    .from('ProductLike')
+    .select('*');
+
+  if (error) {
+
+    console.error('いいね情報の取得に失敗:', error.message);
+    return null;
+
+  } else {
+
+    console.log('いいね情報一覧:', data);
+    return data;
+
+  }
+}
+
+// 動作確認用（実行）
+//getProductLike();
