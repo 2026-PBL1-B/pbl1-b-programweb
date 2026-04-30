050ce008c7ea6dce1f1747c1a7ff42ed711dafe0
OMOCHInoHOSHI <ittamago2020kai@gmail.com>
2026-04-30 13:44:32 +0900

    productLikeからuser_idの引数を削除 issue/52

--- src/api/productLike.js
+++ src/api/productLike.js
@@ -2,13 +2,13 @@ import { supabase } from '../spabase';
 
 /**
  * SpabaseのProductLikeテーブルに新しいいいね情報を追加する関数
+ * user_idはspabaseの機能で自動的に入るため、引数には入れない
  * @param {string} product_id いいねを追加する制作物のID
- * @param {string} user_id いいねを追加するユーザーのID
  */
-export async function postProductLike(product_id, user_id) {
+export async function postProductLike(product_id) {
   const { data, error } = await supabase
     .from('ProductLike')
-    .insert([ { product_id: product_id, user_id: user_id },]);
+    .insert([ { product_id: product_id },]);
 
   if (error) {
     console.error('いいねの追加に失敗:', error.message);
