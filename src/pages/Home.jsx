1f264656eabadad1c2a564010a15ae19c9602006
deruta994 <kd1348760@st.kobedenshi.ac.jp>
2026-05-14 10:41:39 +0900

    stash対応

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@
 import { useNavigate } from "react-router-dom";
 import Header from '../components/Header.jsx';  // ✅ {}を削除
 
-function Home() {  // ✅ Header→Homeに名前変更
+function Header() { 
   return (
     <div>
       <Header />  {/* ✅ importしたHeaderを使う */}
